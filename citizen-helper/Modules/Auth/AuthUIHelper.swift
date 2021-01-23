1b9aa6d04c683e475c8e9238ee0de02e59a63b8c
art-off <tema2707@icloud.com>
2021-01-23 16:16:04 +0700

    now tap on text field wrap view start editing

--- citizen-helper/Modules/Auth/AuthUIHelper.swift
+++ citizen-helper/Modules/Auth/AuthUIHelper.swift
@@ -31,18 +31,43 @@ enum AuthUIHelper {
     }
     
     static func roundedTextFiled() -> (wrapView: UIView, textField: UITextField) {
-        let view = UIView()
         let textField = UITextField()
+        let view = CustomTextField(textField: textField)
         
         view.addSubview(textField)
         textField.snp.makeConstraints { make in
             make.edges.equalToSuperview().inset(10)
         }
         view.backgroundColor = .systemGray5
-        
         view.layer.cornerRadius = 10
         
         return (view, textField)
+    }
+    
+}
+
+class CustomTextField: UIView {
+    
+    let textField: UITextField
+    
+    init(frame: CGRect, textField: UITextField){
+        self.textField = textField
+        super.init(frame: frame)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTap))
+        self.addGestureRecognizer(tap)
+    }
+    
+    convenience init(textField: UITextField) {
+        self.init(frame: .zero, textField: textField)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc
+    private func didTap() {
+        textField.becomeFirstResponder()
     }
     
 }
